17ca32e9b004591e9e0f4bf927d9c69463f44489
Xhriman <seal789ie@gmail.com>
2016-03-20 17:19:39 +0800

    NumPad

--- Calculator/NumberLabel.swift
+++ Calculator/NumberLabel.swift
@@ -7,8 +7,84 @@
 //
 
 import UIKit
+import Foundation
 
 class NumberLabel: UILabel {
-	
 
+	// MARK: - Value
+
+	var value: Double {
+		get {
+			if let text = self.text {
+				return Double(text.stringByReplacingOccurrencesOfString(",", withString: ""))!
+			} else {
+				return 0.0
+			}
+		}
+
+		set {
+			let formatter = NSNumberFormatter()
+			formatter.numberStyle = .DecimalStyle
+			formatter.maximumFractionDigits = 8
+
+			if newValue % 1 == 0 {
+				self.text = formatter.stringFromNumber(Int(newValue))
+			} else if (newValue < 1000) {
+				self.text = "\(Double(newValue))"
+			} else {
+				self.text = formatter.stringFromNumber(Double(newValue))
+			}
+		}
+	}
+
+	func append(digit: Int) {
+		if let text = self.text where text != "0" {
+			if text.containsString(".") {
+				self.text = "\(text)\(digit)"
+			} else {
+				self.value = Double("\(text)\(digit)".stringByReplacingOccurrencesOfString(
+					",", withString: ""))!
+			}
+		} else {
+			self.text = "\(digit)"
+		}
+	}
+
+	func appendFloatPoint() {
+		if let text = self.text where !text.containsString(".") {
+			self.text = "\(text)."
+		}
+	}
+
+	var isInt: Bool {
+		return value % 1 == 0
+	}
+
+	var isNegative: Bool {
+		if let text = self.text {
+			return text.hasPrefix("-")
+		} else {
+			return false
+		}
+	}
+
+	func clear() {
+		self.value = 0.0
+	}
+
+	func changeSign() {
+		guard let text = self.text else {
+			return
+		}
+
+		self.text = self.isNegative
+			? text.substringFromIndex(text.startIndex.advancedBy(1))
+			: "-\(text)"
+	}
+
+	func toPercentage() {
+		if let text = self.text where text != "0" {
+			self.value = value / 100
+		}
+	}
 }
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var numberLabel: NumberLabel!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -17,5 +19,42 @@ class ViewController: UIViewController {
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+
+	@IBAction func numericButtonClicked(sender: AnyObject) {
+		if (sender.tag < 10) {
+			numberLabel.append(sender.tag)
+		} else {
+			numberLabel.appendFloatPoint()
+		}
+	}
+
+	@IBAction func operatorButtonClicked(sender: AnyObject) {
+		switch sender.tag {
+		case 1:
+			print("+")
+		case 2:
+			print("-")
+		case 3:
+			print("x")
+		default:
+			print("/")
+		}
+	}
+
+	@IBAction func clear(sender: AnyObject) {
+		numberLabel.clear()
+	}
+
+	@IBAction func changeSign(sender: AnyObject) {
+		numberLabel.changeSign()
+	}
+
+	@IBAction func convertToPercentage(sender: AnyObject) {
+		numberLabel.toPercentage()
+	}
+
+	@IBAction func calculate(sender: AnyObject) {
+		print(numberLabel.value)
 	}
 }
