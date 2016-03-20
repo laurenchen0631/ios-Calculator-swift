2811ccdd6ab0a8513815aa59ab895d2fa6e7ec21
Xhriman <seal789ie@gmail.com>
2016-03-20 15:04:08 +0800

    Interface

---
+++ Calculator/NumberLabel.swift
@@ -0,0 +1,14 @@
+//
+//  NumberLabel.swift
+//  Calculator
+//
+//  Created by Xhriman on 2016/3/15.
+//  Copyright © 2016年 Exwd. All rights reserved.
+//
+
+import UIKit
+
+class NumberLabel: UILabel {
+	
+
+}
